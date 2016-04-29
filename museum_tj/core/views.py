9715caf8bd01ab9b6ea62e217eb0cee6e1b028b1
dcalsky <dcalsky@163.com>
2016-04-30 00:15:25 +0800

    template update

--- museum_tj/core/views.py
+++ museum_tj/core/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from museum_tj.article.models import Article
+
 def home(request):
-    return render(request, 'core/home.html')
+    news = Article.get_article()
+    return render(request, 'core/home.html', {
+        'news': news
+    })
