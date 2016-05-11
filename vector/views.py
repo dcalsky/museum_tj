b01d89e8b2236d74c3d6536891e8a1ba4b5456c3
dcalsky <dcalsky@163.com>
2016-05-11 23:47:07 +0800

    object not exist

--- vector/views.py
+++ vector/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.exceptions import ObjectDoesNotExist
 
 from vector.models import Article, Part
 
 
 def get_article(request, part_name, article_id):
-    article = Article.objects.filter(id=article_id)
+    try:
+        article = Article.objects.get(id=article_id)
+    except:
+        article = None
     return render(request, 'vector/detail.html', {
         'article': article
     })
@@ -21,11 +25,15 @@ def _get_articles(request, part):
         articles = paginator.page(1)
     except EmptyPage:
         articles = paginator.page(paginator.num_pages)
-    return render(request, 'vector/detail.html', {
-        'articles': articles
+    return render(request, 'vector/part.html', {
+        'articles': articles,
+        'part': part,
     })
 
 
 def get_part(request, part_name):
-    part = Part.objects.get(name=part_name)
+    try:
+        part = Part.objects.get(name=part_name)
+    except ObjectDoesNotExist:
+        part = None
     return _get_articles(request, part)
