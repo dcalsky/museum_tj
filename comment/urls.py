b4bcc9ea715bb1726b79ad65952b2166ed7d686f
dcalsky <dcalsky@163.com>
2016-06-04 01:38:56 +0800

    comment url

--- comment/urls.py
+++ comment/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.get_comments),
+    url(r'^/$', views.get_comments),
     url(r'post$', views.post_comment, name='post'),
     url(r'get$', views.next_comment, name='get'),
 ]
