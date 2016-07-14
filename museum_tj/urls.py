1981cb3b588f49c1388c76be1eaf7e27dd2ef02c
dcalsky <dcalsky@163.com>
2016-07-14 19:17:10 +0800

    part url

--- museum_tj/urls.py
+++ museum_tj/urls.py
@@ -12,9 +12,9 @@ from search import views as view_search
 urlpatterns = [
     url(r'^museum/$', view_home.home, name='home'),
     url(r'admin/', admin.site.urls),
-    url(r'^part/(?P<part_name>\w+)/', include('vector.urls'), name='part'),
+    url(r'part/(?P<part_name>\w+)/', include('vector.urls'), name='part'),
     url(r'^search$', view_search.search, name='search'),
     url(r'^appoint$', view_appoint.apply_appoint, name='appoint'),
-    url(r'^comment', include('comment.urls')),
+    url(r'comment', include('comment.urls')),
     url(r'^summernote/', include('django_summernote.urls')),
 ]
